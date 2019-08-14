62d865d92fe37ed02c1eb3225f8569d989b0b0ff
danny <danny75@gmail.com>
2019-08-14 14:06:18 +0800

    完成XMLParse的測試

---
+++ XMLParse/Models/M2112.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLParse.Models
+{
+    public class M2112
+    {
+        public string MyName { get; set; }
+    }
+}
--- XMLParse/Program.cs
+++ XMLParse/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XMLParse.Models;
 
 namespace XMLParse
 {
@@ -10,6 +15,38 @@ namespace XMLParse
     {
         static void Main(string[] args)
         {
+            string data0000 = GetXMLData("2112");
+        }
+
+
+        /// <summary>
+        /// 取得信件的內容
+        /// </summary>
+        /// <param name="mailInfo"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// </remarks>
+        static string GetXMLData(string txId)
+        {
+            #region 取回樣版檔
+
+            var config = new TemplateServiceConfiguration
+            {
+                TemplateManager = new ResolvePathTemplateManager(new[] { "XMLTemplate" }),
+                DisableTempFileLocking = true
+            };
+
+            Engine.Razor = RazorEngineService.Create(config);
+            string TemplateFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "XMLTemplate");
+
+            string fileName = txId + ".cshtml";
+            var emailTemplatePath = Path.Combine(TemplateFolderPath, fileName);
+            #endregion
+
+            M2112 entity = new M2112() { MyName = "Danny" };
+            string MailBody = Engine.Razor.RunCompile(emailTemplatePath, null, entity);
+
+            return MailBody;
         }
     }
 }
